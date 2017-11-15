4e02c6c0dbbda531e5bac3764dcc4ddf6137d4ac
newwenha <newwenha@gmail.com>
2017-11-15 15:07:43 -0500

    11.15.2017
    Added comment changes to SongSearch

--- src/SongSearch.java
+++ src/SongSearch.java
@@ -1,4 +1,11 @@
+/**
+ * Class that runs the entire project by reading in an input file
+ * 
+ * @author Nguyen Ha (nguyen) and Boyang Li (beyongl)
+ * @version 11.15.2016
+ */
 
-public class SongSearch {
+public class SongSearch
+{
 
 }
